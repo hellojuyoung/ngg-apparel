a0450f3a44c825d6f44e5666de4f0b0bf57bed34
jookushin <newmoon1ight.521@gmail.com>
2020-08-06 02:23:07 +0900

    장바구니, 위시리스트 레이어 추가

--- js/default.js
+++ js/default.js
@@ -3,12 +3,19 @@ function settingHeight() {
     var windowH = $(window).height();
 
     //console.log(windowH);
-    $('.layer-pop').css({
+    $(".layer-pop").css({
         height: windowH + "px"
     });
 }
 
+//layer pop close
+function closeLayer() {
+    $(".layer-pop.on").removeClass("on");
+}
+
+
+
+//run!
 $(document).ready(function () {
     settingHeight();
-
 });
